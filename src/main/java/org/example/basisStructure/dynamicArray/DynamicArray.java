bab150f223acfaf26b39ede0edd0c340b586e116
LiYaoYu <1252505184@qq.com>
2024-05-06 18:13:38 +0800

    initialize

---
+++ src/main/java/org/example/basisStructure/dynamicArray/DynamicArray.java
@@ -0,0 +1,9 @@
+package org.example.basisStructure.dynamicArray;
+
+/**
+ * @Author: erenjaeger
+ * @Date: 2024/5/6 18:11
+ */
+public class DynamicArray {
+
+}
---
+++ src/main/java/org/example/basisStructure/dynamicArray/MyLinkedList.java
@@ -0,0 +1,8 @@
+package org.example.basisStructure.dynamicArray;
+
+/**
+ * @Author: erenjaeger
+ * @Date: 2024/5/6 18:12
+ */
+public class MyLinkedList {
+}
