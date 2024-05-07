98f0f499a9ea36d5b33ad5591fbe0245948b9519
LiYaoYu <1252505184@qq.com>
2024-05-07 22:21:58 +0800

    双链表代码实现第一部分

--- src/main/java/org/example/basisStructure/dynamicArray/MyLinkedList.java
+++ src/main/java/org/example/basisStructure/dynamicArray/MyLinkedList.java
@@ -4,6 +4,110 @@ package org.example.basisStructure.dynamicArray;
  * @Author: erenjaeger
  * @Date: 2024/5/6 18:12
  */
-public class MyLinkedList {
+public class MyLinkedList<E> {
+    // 虚拟头尾节点
+    final private Node<E> head, tail;
+
+    private int size;
+
+    // 双链表节点
+    private static class Node<E> {
+        E val;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(E val) {
+            this.val = val;
+        }
+    }
+
+    // 构造函数初始化虚拟头尾节点
+    public MyLinkedList() {
+        this.head = new Node<>(null);
+        this.tail = new Node<>(null);
+        head.next = tail;
+        tail.prev = head;
+        this.size = 0;
+    }
+
+    /***** 增 *****/
+    public void addLast(E e) {
+        Node<E> x = new Node<>(e);
+        Node<E> temp = tail.prev;
+        // temp <-> tail
+        temp.next = x;
+        x.prev = temp;
+
+        x.next = tail;
+        tail.prev = x;
+        // temp <-> x <-> tail
+        size++;
+    }
+
+    public void addFirst(E e) {
+        Node<E> x = new Node<>(e);
+        Node<E> temp = head.next;
+        // head <-> temp
+        temp.prev = x;
+        x.next = temp;
+
+        head.next = x;
+        x.prev = head;
+        // head <-> x <-> temp
+        size++;
+    }
+
+    public void add(int index, E element) {
+        checkPositionIndex(index);
+        if (index == size) {
+            addLast(element);
+            return;
+        }
+    }
+
+
+    /***** 其他工具函数 *****/
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 检查 index 索引位置是否可以添加元素
+     */
+    private void checkPositionIndex(int index) {
+        if (!isPositionIndex(index)) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    private boolean isPositionIndex(int index) {
+        return index >= 0 && index <= size;
+    }
+
+    /**
+     * 检查 index 索引位置是否可以存在元素
+     */
+    private void checkElementIndex(int index) {
+        if (!isElementIndex(index))
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+    }
+
+    private boolean isElementIndex(int index) {
+        return index >= 0 && index < size;
+    }
+
+    private void display() {
+        System.out.println("size = " + size);
+        for (Node<E> p = head.next; p != tail; p = p.next) {
+            System.out.println(p.val + " - >");
+        }
+        System.out.println("null");
+        System.out.println();
+    }
+
 
 }
---
+++ src/main/java/org/example/basisStructure/dynamicArray/MyLinkedList2.java
@@ -0,0 +1,8 @@
+package org.example.basisStructure.dynamicArray;
+
+/**
+ * @Author: erenjaeger
+ * @Date: 2024/5/7 22:05
+ */
+public class MyLinkedList2 {
+}
